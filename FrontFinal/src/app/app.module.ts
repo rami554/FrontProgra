870a4fbb9b78106f75364719e1499139904a2ed3
juan512 <juandanielpcutile.jc@gmail.com>
2020-06-15 18:22:13 -0400

    Inicio del proyecto

--- FrontFinal/src/app/app.module.ts
+++ FrontFinal/src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { Route, RouterModule } from '@angular/router';
+import { ListarComponent } from './listar/listar.component';
+import { AdicionarComponent } from './adicionar/adicionar.component';
 
+const routes: Route[] = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'adicionar', component: AdicionarComponent},
+  {path: 'listar', component: ListarComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListarComponent,
+    AdicionarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]

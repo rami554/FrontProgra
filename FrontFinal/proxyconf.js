f0965cc2817c529f4d8a2e48d4106f9b47d3b670
juan512 <juandanielpcutile.jc@gmail.com>
2020-06-16 16:29:54 -0400

    lo ultimo 3

---
+++ FrontFinal/proxyconf.js
@@ -0,0 +1,7 @@
+{
+    "/api"; {
+      "target"; "https://localhost:8081",
+      "secure"; false,
+      "changeOrigin"; true
+    }
+}
--- FrontFinal/src/app/service/service.service.ts
+++ FrontFinal/src/app/service/service.service.ts
@@ -7,16 +7,21 @@ import { Obra } from '../models/Obra';
 })
 export class ServiceService {
 
+  token: string = 'dsaybashbaijndaksjdn';
+
   constructor(private http: HttpClient) { }
 
-  Url = 'http://localhost:8081/';
+  Url = 'http://localhost:8080/';
 
   getObras(){
     return this.http.get<Obra[]>(this.Url + 'Obra');
   }
 
   createObras(obra: Obra) {
-    const header = new HttpHeaders({'Content-Type': 'application/json'});
-    return this.http.post(this.Url, obra, {headers: header});
+    const header = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + this.token
+    });
+    return this.http.post(this.Url + 'add', obra, {headers: header});
   }
 }
